325effa0b0c286b70148a017350ed3d96e96ce04
Md Gao <usrgdd@gmail.com>
2012-03-01 05:33:01 +0800

    pause now...

--- newex/newex.js
+++ newex/newex.js
@@ -6,26 +6,31 @@ var urls = $('#Content #topics_index .cell').map(function() {
 	}
 }).get();
 
+for (var i = 0; i < urls.length; i++) {
+	
 // on the topic
 //
 // post
-var title = $('#Content .cell h1').text();
-var author = $('#Content .cell small .dark').text();
-var time = $('#Content .cell small.fade').text();
-var content = $('#Content .topic_content').text();
-var lastUpdated = $($('#Content .cell span.fade')[0]).text().split(' 直到 ')[1];
-//var tag = $('#Content .box .cell .bigger').text().split(' › ')[1].trim();
-var tag = $('#Content .box .cell .bigger a:nth-child(3)').text();
-var tagPath = $('#Content .box .cell .bigger a:nth-child(3)').attr('href');
-// comments
-$('.reply table').map(function() {
-	var $r = $(this);
+var topic = function(id) {
+	$.get('http://www.v2ex.com/t/' + id);
 	return {
-		level: $r.find('.snow').text().split('-')[0].trim().substr(1),
-		time: $r.find('.snow').text().split('-')[1].split(' via ')[0].trim(),
-		device: $r.find('.snow').text().split('-')[1].split(' via ')[1] || "Desktop",
-		author: $r.find('strong .dark').text(),
-		content: $r.find('.reply_content').text()
-	};
-}).get();
+		title: $('#Content .cell h1').text(),
+		author: $('#Content .cell small .dark').text(),
+		time: $('#Content .cell small.fade').text(),
+		content: $('#Content .topic_content').text(),
+		lastUpdated: $($('#Content .cell span.fade')[0]).text().split(' 直到 ')[1],
+		tag: $('#Content .box .cell .bigger a:nth-child(3)').text(),
+		tagPath: $('#Content .box .cell .bigger a:nth-child(3)').attr('href'),
+		comments: $('.reply table').map(function() {
+			var $r = $(this);
+			return {
+				level: $r.find('.snow').text().split('-')[0].trim().substr(1),
+				time: $r.find('.snow').text().split('-')[1].split(' via ')[0].trim(),
+				device: $r.find('.snow').text().split('-')[1].split(' via ')[1] || "Desktop",
+				author: $r.find('strong .dark').text(),
+				content: $r.find('.reply_content').text()
+			};
+		}),
+	}
+}
 
