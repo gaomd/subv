fa064cee7e6f7aa2fc89469721d7c763a7734b0a
Md Gao <usrgdd@gmail.com>
2012-03-02 04:27:54 +0800

    met Cross Domain issue...

--- newex/newex.js
+++ newex/newex.js
@@ -1,11 +1,39 @@
 // at index
-var urls = $('#Content #topics_index .cell').map(function() {
-	var $p = $(this);
-	return {
-		path: $p.find('.bigger a').attr('href'),
-	}
-}).get();
+function update() {
+	$.ajax({
+		url: "http://www.v2ex.com/changes?p=1",
+		success: function(html) {
+			var topics = $(html).find('#Content #topics_index .cell table').map(function() {
+				var $p = $(this);
+				var $meta = $p.find('.created').text();
+				//console.log("splitting " + $meta);
+				return {
+					id: $p.find('.bigger a').attr('href').substr(3).split('#')[0],
+					comments: $p.find('.bigger a').attr('href').substr(3).split('#')[1].replace('reply', ''),
+					title: $p.find('.bigger a').text(),
+					author: $p.find('.created a.dark:nth-child(1)').text(),
+					authorId: $p.parent().attr('class').split(' ').pop().substr(5),
+					tagHumanName: $p.find('.created a.node').text(),
+					tagUnixName: $p.find('.created a.node').attr('href').substr(4),
+					lastCommentor: $meta.split(' ').pop(),
+					// very very unreliable, be careful
+					views: $meta.split('•')[3].split('次点击')[0].trim(),
+					time: $meta.split('•').pop().split('ago')[0].trim(),
+				}
+			}).get();
+			//console.log(topics[0]);
+			for (var i = 0; i < topics.length; i++) {
+				// update page
+				// needed? $('#board').empty();
+				$('#board').html(
+					$('#topic-template').render(topics)
+				);
+			}
+		}
+	});
+}
 
+/*
 for (var i = 0; i < urls.length; i++) {
 	
 // on the topic
@@ -33,4 +61,4 @@ var topic = function(id) {
 		}),
 	}
 }
-
+*/
