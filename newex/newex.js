d108b70fb5c6d5a66ec630ff2f97a4cf24f0bc7e
Md Gao <usrgdd@gmail.com>
2012-03-04 03:22:44 +0800

    fixs indention

--- newex/newex.js
+++ newex/newex.js
@@ -42,31 +42,31 @@ $(function() {
 });
 
 function expandTopic(id) {
-$.get('http://www.v2ex.com/t/' + id, function(html) {
-	// tmp solution, gonna parse img, swf, gist, etc later
-	html = html.replace(/<img\b[^>]*>/ig, '');
-	var topic = {
-		// mainained
-		content: $('#Content .topic_content').text(),
-		comments: $('.reply table').map(function() {
-			var $r = $(this);
-			return {
-				level: $r.find('.snow').text().split('-')[0].trim().substr(1),
-				time: $r.find('.snow').text().split('-')[1].split(' via ')[0].trim(),
-				device: $r.find('.snow').text().split('-')[1].split(' via ')[1] || "Desktop",
-				author: $r.find('strong .dark').text(),
-				content: $r.find('.reply_content').text()
-			};
-		}),
-		// not needed
-		//title: $('#Content .cell h1').text(),
-		//author: $('#Content .cell small .dark').text(),
-		//time: $('#Content .cell small.fade').text(),
-		//lastUpdated: $($('#Content .cell span.fade')[0]).text().split(' 直到 ')[1],
-		//tag: $('#Content .box .cell .bigger a:nth-child(3)').text(),
-		//tagPath: $('#Content .box .cell .bigger a:nth-child(3)').attr('href'),
-	}
-	$('#board #' + id + ' .commentarea').html($('#comment-template').render(topic));
-});
+	$.get('http://www.v2ex.com/t/' + id, function(html) {
+		// tmp solution, gonna parse img, swf, gist, etc later
+		html = html.replace(/<img\b[^>]*>/ig, '');
+		var topic = {
+			// mainained
+			content: $('#Content .topic_content').text(),
+			comments: $('.reply table').map(function() {
+				var $r = $(this);
+				return {
+					level: $r.find('.snow').text().split('-')[0].trim().substr(1),
+					time: $r.find('.snow').text().split('-')[1].split(' via ')[0].trim(),
+					device: $r.find('.snow').text().split('-')[1].split(' via ')[1] || "Desktop",
+					author: $r.find('strong .dark').text(),
+					content: $r.find('.reply_content').text()
+				};
+			}),
+			// not needed
+			//title: $('#Content .cell h1').text(),
+			//author: $('#Content .cell small .dark').text(),
+			//time: $('#Content .cell small.fade').text(),
+			//lastUpdated: $($('#Content .cell span.fade')[0]).text().split(' 直到 ')[1],
+			//tag: $('#Content .box .cell .bigger a:nth-child(3)').text(),
+			//tagPath: $('#Content .box .cell .bigger a:nth-child(3)').attr('href'),
+		}
+		$('#board #' + id + ' .commentarea').html($('#comment-template').render(topic));
+	});
 }
 
