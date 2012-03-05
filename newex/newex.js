ee63ce1adc6a15febe8be03e1a391ef50d84989f
Md Gao <usrgdd@gmail.com>
2012-03-06 03:13:28 +0800

    added some helper function, some code formatting

--- newex/newex.js
+++ newex/newex.js
@@ -1,49 +1,84 @@
+function isVisited(tid, ncomments) {
+	var t = localStorage["topic" + tid];
+	if (t && t.visited) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function hasNewComments(tid, ncommentsNow) {
+	var t = localStorage["topic" + tid];
+	if (t.ncomments !== ncommentsNow) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function visited(tid, ncomments) {
+	var t = localStorage["topic" + tid];
+	t.ncomments = ncomments;
+	t.visited = true;
+}
+
+function cacheTopic(tid, data) {
+	var t = localStorage["topic" + tid];
+	t.cache = data;
+}
+
+function cacheTopics(pageNo, data) {
+	var p = localStorage["page" + pageNo];
+	p = data;
+}
+
 
 function updateBoard(pageNo) {
 	console.log("updating");
-	$.ajax({
-		url: "http://www.v2ex.com/changes?p=" + +pageNo,
-		success: function(html) {
-			// http://stackoverflow.com/a/6485092/753533 explains why do replace
-			html = html.replace(/<img\b[^>]*>/ig, '');
-			var topics = $(html).find('#Content #topics_index .cell table').map(function() {
-				var $p = $(this);
-				var $meta = $p.find('.created').text();
-				//console.log("splitting " + $meta);
-				return {
-					id: $p.find('.bigger a').attr('href').substr(3).split('#')[0],
-					comments: $p.find('.bigger a').attr('href').substr(3).split('#')[1].replace('reply', ''),
-					title: $p.find('.bigger a').text(),
-					author: $p.find('.created a.dark:nth-child(1)').text(),
-					authorId: $p.parent().attr('class').split(' ').pop().substr(5),
-					tagHumanName: $p.find('.created a.node').text(),
-					tagUnixName: $p.find('.created a.node').attr('href').substr(4),
-					lastCommentor: $meta.split(' ').pop(),
-					// very very unreliable, be careful
-					// TODO views: $meta.split('•')[3].split('次点击')[0].trim(),
-					// wont split the bullet?
-					// TODO time: $meta.split('&bullet;').pop().split('ago')[0].trim()
-				}
-			}).get();
-			//console.log(topics[0]);
-			for (var i = 0; i < topics.length; i++) {
-				// update page
-				// needed? $('#board').empty();
-				$('#board').html(
-					$('#topic-template').render(topics)
-				);
+	console.log(new Date() + ": GETting");
+	$.get("http://www.v2ex.com/changes?p=" + +pageNo, function(data) {
+		// http://stackoverflow.com/a/6485092/753533 explains why do replace
+		var html = data.responseText.replace(/<img\b[^>]*>/ig, '');
+		var topics = $(html).find('#Content #topics_index .cell table').map(function() {
+			var $p = $(this);
+			var $meta = $p.find('.created').text();
+			//console.log("splitting " + $meta);
+			return {
+				id: $p.find('.bigger a').attr('href').substr(3).split('#')[0],
+				comments: $p.find('.bigger a').attr('href').substr(3).split('#')[1].replace('reply', ''),
+				title: $p.find('.bigger a').text(),
+				author: $p.find('.created a.dark:nth-child(1)').text(),
+				authorId: $p.parent().attr('class').split(' ').pop().substr(5),
+				tagHumanName: $p.find('.created a.node').text(),
+				tagUnixName: $p.find('.created a.node').attr('href').substr(4),
+				lastCommentor: $meta.split(' ').pop(),
+				// very very unreliable, be careful
+				// TODO views: $meta.split('•')[3].split('次点击')[0].trim(),
+				// wont split the bullet?
+				// TODO time: $meta.split('&bullet;').pop().split('ago')[0].trim()
 			}
+		}).get();
+		console.log(new Date() + ": got");
+		//console.log(topics[0]);
+		for (var i = 0; i < topics.length; i++) {
+			// update page
+			// needed? $('#board').empty();
+			$('#board').html(
+				$('#topic-template').render(topics)
+			);
 		}
+		console.log(new Date() + ": rendered");
 	});
 }
 
 function expandTopic(id) {
-	$.get('http://www.v2ex.com/t/' + id, function(html) {
+	$.get('http://www.v2ex.com/t/' + id, function(data) {
+		console.log(new Date() + ": GETting");
 		// tmp solution, gonna parse img, swf, gist, etc later
-		html = html.replace(/<img\b[^>]*>/ig, '');
+		var html = data.responseText.replace(/<img\b[^>]*>/ig, '');
 		var topic = {
 			// mainained
-			content: $(html).find('#Content .topic_content').text(),
+			content: $(html).find('#Content .topic_content').html(),
 			comments: $(html).find('.reply table').map(function() {
 				var $r = $(this);
 				return {
@@ -53,7 +88,7 @@ function expandTopic(id) {
 					time: $r.find('.snow').text().split('-')[1].split(' via ')[0].trim(), // TODO remove ago
 					device: $r.find('.snow').text().split('-')[1].split(' via ')[1] || "Desktop",
 					author: $r.find('strong .dark').text(),
-					content: $r.find('.reply_content').text()
+					content: $r.find('.reply_content').html(),
 				};
 			})
 			// not needed
@@ -64,15 +99,19 @@ function expandTopic(id) {
 			//tag: $('#Content .box .cell .bigger a:nth-child(3)').text(),
 			//tagPath: $('#Content .box .cell .bigger a:nth-child(3)').attr('href'),
 		}
-		console.log('appending comments');
-		console.log('id is: ' + id);
-		console.log('content is: ' + topic.content);
-		console.log('comments[0] is: ' + topic.comments[0]);
-		$('#board #' + id).addClass('expanded');
-		$('#board #' + id + ' > .content').html(topic.content);
-		for (var i = 0; i < topic.comments.length; i++) {
-			$($('#comment-template').render(topic.comments[i])).appendTo('#board #' + id + ' .commentarea');
+		console.log(new Date() + ": got");
+		//console.log('appending comments');
+		//console.log('id is: ' + id);
+		//console.log('content is: ' + topic.content);
+		//console.log('comments[0] is: ' + topic.comments[0].content);
+		//$('#board #' + id).addClass('expanded');
+		//$('#board #' + id + ' > .content').html(topic.content);
+		for (var i = topic.comments.length - 1; i >= 0; i--) {
+			$('#board #' + id + ' .commentarea').append(
+				$('#comment-template').render(topic.comments[i])
+			);
 		}
+		console.log(new Date() + ": rendered");
 	});
 }
 
