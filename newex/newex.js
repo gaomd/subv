57c5c6c219f83c2f9dc349491a9a7808493d0bd2
Md Gao <usrgdd@gmail.com>
2012-03-07 20:43:04 +0800

    major refactor, fixed various HTML -> JSON bugs, UNTESTED COMMIT

--- newex/newex.js
+++ newex/newex.js
@@ -1,61 +1,29 @@
-function isVisited(tid, ncomments) {
-	var t = localStorage["topic" + tid];
-	if (t && t.visited) {
-		return true;
-	} else {
-		return false;
+var pref = {
+	debug: true,
+}
+
+function debug(o) {
+	if (pref.debug) {
+		console.log(o);
 	}
 }
 
-function hasNewComments(tid, ncommentsNow) {
-	var t = localStorage["topic" + tid];
-	if (t.ncomments !== ncommentsNow) {
-		return true;
-	} else {
-		return false;
-	}
-}
-
-function visited(tid, ncomments) {
-	var t = localStorage["topic" + tid];
-	t.ncomments = ncomments;
-	t.visited = true;
-}
-
-function updateBoard(pageNo) {
-	//console.log("updating");
-	//console.log(new Date() + ": GETting");
+function loadTopicList(pageNo) {
 	$.get("http://www.v2ex.com/changes?p=" + +pageNo, function(html) {
-		//console.log(new Date() + ": got, parsing");
 		var topicList = topicListParse(html);
-		//console.log(new Date() + ": parsed in JSON");
-		//console.log(topics[0]);
-		//console.log("items count: " + topics.length);
-		renderTopicList(topicList);
+		debug(topicList);
 		for (var i = topicList.length - 1; i >= 0; i--) {
-			// update page
-			// needed? $('#board').empty();
 			$('#board').append(
 				$('#topic-template').render(topicList[i])
 			);
 		}
-		//console.log(new Date() + ": rendered");
 	});
 }
 
-function renderTopicList() {
-}
-
-function expandTopic(id) {
-	console.log(new Date() + ": GETting");
+function loadTopic(id) {
 	$.get('http://www.v2ex.com/t/' + id, function(html) {
-		console.log(new Date() + ": got, parsing");
-		console.log(new Date() + ": parsed into JSON");
 		var topic = topicParse(html);
-		//console.log('appending comments');
-		console.log('id is: ' + id);
-		console.log('content is: ' + topic.content);
-		console.log('comments[0] is: ' + topic.comments[0].content);
+		debug(topic);
 		$('#board #' + id).addClass('expanded');
 		$('#board #' + id + ' > .content').html(topic.content);
 		for (var i = topic.comments.length - 1; i >= 0; i--) {
@@ -63,7 +31,6 @@ function expandTopic(id) {
 				$('#comment-template').render(topic.comments[i])
 			);
 		}
-		console.log(new Date() + ": rendered");
 	});
 }
 
@@ -81,66 +48,84 @@ function killImgTag(text) {
 	return text.replace(/<img\b.*?src="(.*?)".*?>.*?<\/img>/ig, '$1');
 }
 
-// the nauty part... extract HTML text into JSON
+// the nasty part... extract HTML text into JSON
 function topicParse(html) {
 	// tmp solution, gonna parse swf, gist, etc later
 	var html = killImgTag(extractResponseText(html));
 	var $dom = $(html);
-	var topic {
-		id: $dom.find('form[method="post"]').attr('action').replace('/t/', ''),
+	var topic = {
+		// following is same as in topicList but more up-to-date
+		topicId: $dom.find('form[method="post"]').attr('action').replace('/t/', ''),
+		commentsCount: $dom.find('.bigger a').attr('href').substr(3).split('#')[1].replace('reply', ''),
+		title: $dom.find('#Content .cell h1').text(),
+		author: $dom.find('#Content .cell small .dark').text(),
+		authorId: null, // TODO
+		tagId: $dom.find('#Content .box .cell .bigger a:nth-child(3)').attr('href'),
+		tagName: $dom.find('#Content .box .cell .bigger a:nth-child(3)').text(),
+		lastCommentor: null, // TODO
+		hasContent: false, // TODO
+		// unreliable because of caching
+		viewsCount: viewsCount,
+		timeAgo: $dom.find('#Content .cell small.fade').text(),
+		// same thing ends
+
+		// TODO lastActivity: $($('#Content .cell span.fade')[0]).text().split(' 直到 ')[1],
 		content: $dom.find('#Content .topic_content').html(),
 		comments: $dom.find('.reply table').map(function() {
-			var $r = $(this);
+			var $this = $(this);
 			return {
-				tid: id,
-				id: undefined, // TODO
-				level: $r.find('.snow').text().split('-')[0].trim().substr(1),
-				time: $r.find('.snow').text().split('-')[1].split(' via ')[0].trim(), // TODO remove ago
-				device: $r.find('.snow').text().split('-')[1].split(' via ')[1] || "Desktop",
-				author: $r.find('strong .dark').text(),
-				content: $r.find('.reply_content').html(),
+				order: $this.find('.snow').text().split('-')[0].trim().substr(1),
+				timeAgo: $this.find('.snow').text().split('-')[1].split(' via ')[0].trim(),
+				device: $this.find('.snow').text().split('-')[1].split(' via ')[1] || "Desktop",
+				author: $this.find('strong .dark').text(),
+				authorId: $this.parent().attr('class').split('_').pop(),
+				commentId: $this.find('td:nth-child(3) > div').attr('id').split('_')[1],
+				content: $this.find('.reply_content').html(),
 			};
-		})
-		// not needed
-		//title: $('#Content .cell h1').text(),
-		//author: $('#Content .cell small .dark').text(),
-		//time: $('#Content .cell small.fade').text(),
-		//lastUpdated: $($('#Content .cell span.fade')[0]).text().split(' 直到 ')[1],
-		//tag: $('#Content .box .cell .bigger a:nth-child(3)').text(),
-		//tagPath: $('#Content .box .cell .bigger a:nth-child(3)').attr('href'),
+		}),
 	}
 }
 
-// the nauty part... extract HTML text into JSON
+// the nasty part... extract HTML text into JSON
 function topicListParse(html) {
 	var html = killImgTag(extractResponseText(html));
 	var $dom = $(html);
-	var topicList;
-	topicList.page = $dom.find(.page_current').text();
+	var topicList = {};
+	topicList.page = $dom.find('.page_current').text();
 	topicList.items = $dom.find('#Content #topics_index .cell table').map(function() {
 		var $p = $(this);
-		var meta = $p.find('.created').text().split('•');
-		var viewsCount, timeAgo, titleOnly;
-		if (meta.length === 4) {
-			titleOnly = true;
-			viewsCount = meta[2].split('次点击')[0].trim();
-			timeAgo = meta[3].split(' replied ').shift().trim();
-		} else if (meta.length === 5) {
-			titleOnly = false;
-			viewsCount = meta[3].split('次点击')[0].trim();
-			timeAgo = meta[4].split(' replied ').shift().trim();
+
+		var meta = $p.find('.created').text().split(/replied by|•/);
+		// possible meta format
+		// ["DOMAIN", "AUTHOR", "CHAR_COUNT", "VIEWS", "TIME_AGO", "LAST_COMMENTOR"]
+		// ["DOMAIN", "AUTHOR", "CHAR_COUNT", "VIEWS", "TIME_AGO"]
+		// ["DOMAIN", "AUTHOR", "VIEWS", "TIME_AGO", "LAST_COMMENTOR"]
+		// ["DOMAIN", "AUTHOR", "VIEWS", "TIME_AGO"]
+		var viewsCount, timeAgo, hasContent, lastCommentor;
+		meta.shift();
+		meta.shift();
+		if (/个字符/.test(meta[0])) {
+			hasContent = true;
+			meta.shift();
 		}
+		viewsCount = meta.shift().split("次点击").shift().trim();
+		timeAgo = meta.shift().trim();
+		if (meta.length === 0) {
+			lastCommentor = "(nobody)";
+		} else {
+			lastCommentor = meta.shift().trim();
+		}
+
 		return {
-			page: $p.find('.page_current').text(),
-			id: $p.find('.bigger a').attr('href').substr(3).split('#')[0],
+			topicId: $p.find('.bigger a').attr('href').substr(3).split('#')[0],
 			commentsCount: $p.find('.bigger a').attr('href').substr(3).split('#')[1].replace('reply', ''),
 			title: $p.find('.bigger a').text(),
 			author: $p.find('.created a.dark:nth-child(1)').text(),
 			authorId: $p.parent().attr('class').split(' ').pop().substr(5),
 			tagId: $p.find('.created a.node').attr('href').substr(4),
 			tagName: $p.find('.created a.node').text(),
-			lastCommentor: $meta.split(' ').pop(),
-			titleOnly: titleOnly,
+			lastCommentor: lastCommentor,
+			hasContent: hasContent,
 			// unreliable because of caching
 			viewsCount: viewsCount,
 			timeAgo: timeAgo,
@@ -150,8 +135,6 @@ function topicListParse(html) {
 }
 
 $(function() {
-	updateBoard(1);
-	chrome.browserAction.setBadgeText({ text: '111' });
-	chrome.browserAction.setBadgeBackgroundColor({ color: [150, 210, 100, 200] });
+	loadTopicList(1);
 });
 
